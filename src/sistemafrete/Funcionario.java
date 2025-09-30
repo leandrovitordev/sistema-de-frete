1658d5c65c0a57666ad47efa68aae9e65b031122
leandrovitordev <leandro.silva.016@ufrn.edu.br>
2025-09-29 21:42:52 -0300

    Update Funcionario.java

--- src/sistemafrete/Funcionario.java
+++ src/sistemafrete/Funcionario.java
@@ -1,11 +1,11 @@
 package sistemafrete;
 
 // Funcionario.java
-public class Funcionario extends Entregador implements Fretavel { // [cite: 104, 106]
+public class Funcionario extends Entregador implements Fretavel { 
 
     @Override
     public double calcularCustoFrete(double distanciaEmKm) {
-        // Custo de um funcionário é fixo em R$ 5,00 por entrega [cite: 104]
+        // Custo de um funcionário é fixo em R$ 5,00 por entrega 
         return 5.00;
     }
 }
